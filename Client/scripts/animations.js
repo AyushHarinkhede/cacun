e5f363c17c3fb9de8517eff7bd40f824713dd915
Ayush Harinkhede <ayushharinkhere2005@gmail.com>
2026-01-11 20:20:02 +0530

    cacun

--- Client/scripts/animations.js
+++ Client/scripts/animations.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Intersection Observer for scroll animations
   const observerOptions = {
     threshold: 0.1,
-    rootMargin: '0px 0px -50px 0px'
+    rootMargin: '0px 0px -50px 0px' 
   };
 
   const observer = new IntersectionObserver((entries) => {
