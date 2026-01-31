bdba9a38c22f22d60a1384352a9bfffdf27278aa
Ayush Harinkhede <ayushharinkhere2005@gmail.com>
2026-01-31 16:46:59 +0530

    Update replace-colors.sh

--- scripts/replace-colors.sh
+++ scripts/replace-colors.sh
@@ -5,3 +5,4 @@ find . -type f -name "*.css" -print0 | xargs -0 sed -i \
   -e 's/#\([0-9A-Fa-f]\)\{3,6\}/REVIEW_HEX/g' \
   -e 's/rgb([^)]*)/REVIEW_RGB/g'
 echo "Done. Search for REVIEW_HEX/REVIEW_RGB and manually replace with var(--light-blue) or var(--light-yellow) as appropriate."
+
